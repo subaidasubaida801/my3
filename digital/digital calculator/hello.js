e60f31df576eecf6151b52124fa7c94d3e8de528
subaidasubaida801 <92269327+subaidasubaida801@users.noreply.github.com>
2023-01-20 15:59:13 -0800

    Add files via upload

---
+++ digital/digital calculator/hello.js
@@ -0,0 +1,33 @@
+
+
+var screen=document.getElementById("screen");
+
+
+
+function btnClick(value){
+screen.value+=value;
+
+
+}
+
+function clearScreen(){
+screen.value="";
+}
+
+
+
+
+
+
+
+
+function findResult(){
+var result = eval(screen.value)
+screen.value = result;
+
+
+
+ 
+}
+
+
